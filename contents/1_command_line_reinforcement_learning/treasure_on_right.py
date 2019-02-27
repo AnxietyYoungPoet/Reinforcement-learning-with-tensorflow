df59d30e5d5b96708fc91c99da6ded9cd779099e
SunPeiquan <spq1994@hotmail.com>
2019-02-27 12:14:51 +0800

    mod on_right

--- contents/1_command_line_reinforcement_learning/treasure_on_right.py
+++ contents/1_command_line_reinforcement_learning/treasure_on_right.py
@@ -14,55 +14,45 @@ np.random.seed(2)  # reproducible
 
 
 N_STATES = 6   # the length of the 1 dimensional world
-ACTIONS = ['left', 'right']     # available actions
+TERMINAL = N_STATES - 1
+ACTIONS = [0, 1]     # -1 for left and 1 for right
 EPSILON = 0.9   # greedy police
-ALPHA = 0.1     # learning rate
+ALPHA = 0.2     # learning rate
 GAMMA = 0.9    # discount factor
-MAX_EPISODES = 13   # maximum episodes
+MAX_EPISODES = 10   # maximum episodes
 FRESH_TIME = 0.3    # fresh time for one move
 
 
 def build_q_table(n_states, actions):
-    table = pd.DataFrame(
-        np.zeros((n_states, len(actions))),     # q_table initial values
-        columns=actions,    # actions's name
-    )
-    # print(table)    # show table
+    table = np.zeros((n_states, actions))
     return table
 
 
 def choose_action(state, q_table):
     # This is how to choose an action
-    state_actions = q_table.iloc[state, :]
-    if (np.random.uniform() > EPSILON) or ((state_actions == 0).all()):  # act non-greedy or state-action have no value
-        action_name = np.random.choice(ACTIONS)
+    state_actions = q_table[state, :]
+    if (np.random.uniform() > EPSILON):  # act non-greedy or state-action have no value
+        action = np.random.choice(ACTIONS)
     else:   # act greedy
-        action_name = state_actions.idxmax()    # replace argmax to idxmax as argmax means a different function in newer version of pandas
-    return action_name
+        action = np.argmax(state_actions)   # replace argmax to idxmax as argmax means a different function in newer version of pandas
+    return action
 
 
-def get_env_feedback(S, A):
+def get_env_feedback(state, action):
     # This is how agent will interact with the environment
-    if A == 'right':    # move right
-        if S == N_STATES - 2:   # terminate
-            S_ = 'terminal'
-            R = 1
-        else:
-            S_ = S + 1
-            R = 0
-    else:   # move left
-        R = 0
-        if S == 0:
-            S_ = S  # reach the wall
-        else:
-            S_ = S - 1
-    return S_, R
+    action = int(action * 2 - 1)
+    next_state = max(0, state + action)
+    if next_state == TERMINAL:
+        reward = 0
+    else:
+        reward = -1
+    return next_state, reward
 
 
 def update_env(S, episode, step_counter):
     # This is how environment be updated
     env_list = ['-']*(N_STATES-1) + ['T']   # '---------T' our environment
-    if S == 'terminal':
+    if S == TERMINAL:
         interaction = 'Episode %s: total_steps = %s' % (episode+1, step_counter)
         print('\r{}'.format(interaction), end='')
         time.sleep(2)
@@ -76,24 +66,20 @@ def update_env(S, episode, step_counter):
 
 def rl():
     # main part of RL loop
-    q_table = build_q_table(N_STATES, ACTIONS)
+    q_table = build_q_table(N_STATES, len(ACTIONS))
     for episode in range(MAX_EPISODES):
         step_counter = 0
         S = 0
-        is_terminated = False
         update_env(S, episode, step_counter)
-        while not is_terminated:
+        while not S == TERMINAL:
 
             A = choose_action(S, q_table)
             S_, R = get_env_feedback(S, A)  # take action & get next state and reward
-            q_predict = q_table.loc[S, A]
-            if S_ != 'terminal':
-                q_target = R + GAMMA * q_table.iloc[S_, :].max()   # next state is not terminal
-            else:
-                q_target = R     # next state is terminal
-                is_terminated = True    # terminate this episode
+            q_predict = q_table[S, A]
+            q_target = R + GAMMA * np.max(q_table[S_, :])
+            # print(q_target, q_table[S_, :])
 
-            q_table.loc[S, A] += ALPHA * (q_target - q_predict)  # update
+            q_table[S, A] += ALPHA * (q_target - q_predict)  # update
             S = S_  # move to next state
 
             update_env(S, episode, step_counter+1)
