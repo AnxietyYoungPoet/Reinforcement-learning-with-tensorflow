d6f04abe716499ef7fd320adb8efc59e28b2c77f
SunPeiquan <spq1994@hotmail.com>
2019-03-04 16:50:07 +0800

    DQN finished

--- contents/5.2_Prioritized_Replay_DQN/run_MountainCar.py
+++ contents/5.2_Prioritized_Replay_DQN/run_MountainCar.py
@@ -11,6 +11,8 @@ gym: 0.8.0
 
 import gym
 from RL_brain import DQNPrioritizedReplay
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
@@ -22,48 +24,48 @@ MEMORY_SIZE = 10000
 
 sess = tf.Session()
 with tf.variable_scope('natural_DQN'):
-    RL_natural = DQNPrioritizedReplay(
-        n_actions=3, n_features=2, memory_size=MEMORY_SIZE,
-        e_greedy_increment=0.00005, sess=sess, prioritized=False,
-    )
+  RL_natural = DQNPrioritizedReplay(
+    n_actions=3, n_features=2, memory_size=MEMORY_SIZE,
+    e_greedy_increment=0.00005, sess=sess, prioritized=False,
+  )
 
 with tf.variable_scope('DQN_with_prioritized_replay'):
-    RL_prio = DQNPrioritizedReplay(
-        n_actions=3, n_features=2, memory_size=MEMORY_SIZE,
-        e_greedy_increment=0.00005, sess=sess, prioritized=True, output_graph=True,
-    )
+  RL_prio = DQNPrioritizedReplay(
+    n_actions=3, n_features=2, memory_size=MEMORY_SIZE,
+    e_greedy_increment=0.00005, sess=sess, prioritized=True, output_graph=True,
+  )
 sess.run(tf.global_variables_initializer())
 
 
 def train(RL):
-    total_steps = 0
-    steps = []
-    episodes = []
-    for i_episode in range(20):
-        observation = env.reset()
-        while True:
-            # env.render()
+  total_steps = 0
+  steps = []
+  episodes = []
+  for i_episode in range(20):
+    observation = env.reset()
+    while True:
+      # env.render()
 
-            action = RL.choose_action(observation)
+      action = RL.choose_action(observation)
 
-            observation_, reward, done, info = env.step(action)
+      observation_, reward, done, info = env.step(action)
 
-            if done: reward = 10
+      if done: reward = 10
 
-            RL.store_transition(observation, action, reward, observation_)
+      RL.store_transition(observation, action, reward, observation_)
 
-            if total_steps > MEMORY_SIZE:
-                RL.learn()
+      if total_steps > MEMORY_SIZE:
+        RL.learn()
 
-            if done:
-                print('episode ', i_episode, ' finished')
-                steps.append(total_steps)
-                episodes.append(i_episode)
-                break
+      if done:
+        print('episode ', i_episode, ' finished')
+        steps.append(total_steps)
+        episodes.append(i_episode)
+        break
 
-            observation = observation_
-            total_steps += 1
-    return np.vstack((episodes, steps))
+      observation = observation_
+      total_steps += 1
+  return np.vstack((episodes, steps))
 
 his_natural = train(RL_natural)
 his_prio = train(RL_prio)
